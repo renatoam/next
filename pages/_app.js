b49d24c633c017005965c044ac273995a717099a
Renato Melo <renato.melo@redspark.io>
2021-01-28 11:00:09 -0300

    feature: create project configuration and install main development tools

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,25 @@
-import '../styles/globals.css'
+import PropTypes from 'prop-types'
+import { ThemeProvider } from 'styled-components'
+import { GlobalStyle } from '../styles/global'
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+const theme = {
+  colors: {
+    primary: '#0070f3'
+  }
 }
 
-export default MyApp
+export default function App({ Component, pageProps }) {
+  return (
+    <>
+      <GlobalStyle />
+      <ThemeProvider theme={theme}>
+        <Component {...pageProps} />
+      </ThemeProvider>
+    </>
+  )
+}
+
+App.propTypes = {
+  Component: PropTypes.elementType,
+  pageProps: PropTypes.object
+}
